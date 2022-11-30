22505bfe485b9c6996ee73323a8b4e1bf031b5f7
calbertan <02c.albert@gmail.com>
2022-11-29 21:04:30 -0800

    combined user and ticket database

--- Project/app/src/main/java/com/example/myapplication/UserTicketDatabase.kt
+++ Project/app/src/main/java/com/example/myapplication/UserTicketDatabase.kt
@@ -5,21 +5,21 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Ticket::class], version = 1)
-abstract class TicketDatabase : RoomDatabase() { //XD: Room automatically generates implementations of your abstract TicketDatabase class.
-    abstract val ticketDatabaseDao: TicketDatabaseDao
+@Database(entities = [User::class, Ticket::class], version = 1)
+abstract class TicketAndUserDatabase : RoomDatabase() { //XD: Room automatically generates implementations of your abstract UserDatabase class.
+//    abstract val userDatabaseDao: UserDatabaseDao
 
     companion object{
         //The Volatile keyword guarantees visibility of changes to the INSTANCE variable across threads
         @Volatile
-        private var INSTANCE: TicketDatabase? = null
+        private var INSTANCE: TicketAndUserDatabase? = null
 
-        fun getInstance(context: Context) : TicketDatabase{
+        fun getInstance(context: Context) : TicketAndUserDatabase{
             synchronized(this){
                 var instance = INSTANCE
                 if(instance == null){
                     instance = Room.databaseBuilder(context.applicationContext,
-                        TicketDatabase::class.java, "ticket_table").build()
+                        TicketAndUserDatabase::class.java, "user_ticket_table").build()
                     INSTANCE = instance
                 }
                 return instance
