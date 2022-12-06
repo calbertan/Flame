04d5c7c56ee5a6488dfea38c13f9505245c35332
calbertan <02c.albert@gmail.com>
2022-12-05 16:18:18 -0800

    upload image

--- Project/app/src/main/java/com/example/myapplication/Add.kt
+++ Project/app/src/main/java/com/example/myapplication/Add.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication
 
+import android.app.Activity.RESULT_OK
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -18,12 +22,15 @@ import com.example.myapplication.Database.*
 import com.example.myapplication.Database.Entities.Ticket
 import com.example.myapplication.Database.Entities.User
 import kotlinx.coroutines.launch
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 
 class Add: Fragment() {
     private lateinit var databaseDao: UserDatabaseDao
     private lateinit var repository: UserRepository
     private lateinit var factory: UserViewModelFactory
     private lateinit var viewModel: UserViewModel
+    private lateinit var uploadImage: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,9 +39,9 @@ class Add: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.publish, container, false)
 
-        val uploadImage:ImageView = view.findViewById(R.id.rectangle_14)
+        uploadImage = view.findViewById(R.id.photo_background)
         uploadImage.setOnClickListener(){
-
+            pickImageGallery()
         }
 
         val readyButton: TextView = view.findViewById(R.id.Ready)
@@ -112,7 +119,7 @@ class Add: Fragment() {
                       userId = 0L,
                       buyerId = -1L,
                       sellerId = currentid,
-                      ticketPhoto = getBitmap()
+                      ticketPhoto = compressImage(uploadImage.drawable.toBitmap())!!
                   )
                   viewModel.insertTicket(ticket)
                 }
@@ -133,4 +140,62 @@ class Add: Fragment() {
         return (result as BitmapDrawable).bitmap
     }
 
+    private fun pickImageGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == RESULT_OK){
+            uploadImage.setImageURI(data?.data)
+        }
+    }
+
+//    fun resizePhoto(bitmap: Bitmap): Bitmap {
+//        val w = bitmap.width
+//        val h = bitmap.height
+//        val aspRat = w / h
+//        val H = 110
+//        val W = H * aspRat
+//        val b = Bitmap.createScaledBitmap(bitmap, W, H, false)
+//
+//        return b
+//
+//    }
+private fun compressImage(image: Bitmap): Bitmap? {
+    val baos = ByteArrayOutputStream()
+    image.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+    var options = 90
+    val length = baos.toByteArray().size / 1024
+    if (length > 5000) {
+        //重置baos即清空baos
+        baos.reset()
+        //质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+        image.compress(Bitmap.CompressFormat.JPEG, 10, baos)
+    } else if (length > 4000) {
+        baos.reset()
+        image.compress(Bitmap.CompressFormat.JPEG, 20, baos)
+    } else if (length > 3000) {
+        baos.reset()
+        image.compress(Bitmap.CompressFormat.JPEG, 50, baos)
+    } else if (length > 2000) {
+        baos.reset()
+        image.compress(Bitmap.CompressFormat.JPEG, 70, baos)
+    }
+    //循环判断如果压缩后图片是否大于1M,大于继续压缩
+    while (baos.toByteArray().size / 1024 > 1024) {
+        //重置baos即清空baos
+        baos.reset()
+        //这里压缩options%，把压缩后的数据存放到baos中
+        image.compress(Bitmap.CompressFormat.JPEG, options, baos)
+        //每次都减少10
+        options -= 10
+    }
+    //把压缩后的数据baos存放到ByteArrayInputStream中
+    val isBm = ByteArrayInputStream(baos.toByteArray())
+    //把ByteArrayInputStream数据生成图片
+    return BitmapFactory.decodeStream(isBm, null, null)
+}
 }
